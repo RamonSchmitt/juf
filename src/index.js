d99b7faf11d22bd5204b9babf15de31083d3c91b
RamonSchmitt <ramon.schmitt@yahoo.com>
2016-12-28 22:07:05 +0100

    bug fix

--- src/index.js
+++ src/index.js
@@ -1,22 +1,11 @@
-var number = process.argv.slice(2)[0];
-
-if(isJuf(number)){
-    console.log("juf!");
-  } else {
-    console.log(number);
-  }
-
-function isJuf(number) {
-  if (number % 7 === 0) {
-    return isJuf = true
-  } else if (number % 10 === 7){
-    return isJuf = true
-  }
-  else {
-     return isJuf = false
-  };
-}
-
 module.exports = {
-  isJuf: isJuf,
+  isJuf: function(number) {
+    if (number % 7 === 0) {
+      return isJuf = true;
+    } else if (number % 10 === 7) {
+      return isJuf = true;
+    } else {
+      return isJuf = false;
+    }
+  }
 };
